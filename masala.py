66dc2717d8792f9a16f130e0153aaf4ee3b1d675
BEHRUZBEK JUMABOYEV <jumaboyevbehruz148@gmail.com>
2025-09-19 16:26:59 +0500

    luboy narsa

---
+++ masala.py
@@ -0,0 +1,36 @@
+# son = int(input("sonni kiriting :"))
+# if son>0:
+#     print(f"{son} soni musbat son .")
+# elif son < 0 :
+#     print(f"{son} manfiy son")
+# else:
+#     print("son nolga teng")
+
+# son1 = int(input("sonni kieiting  :" ))
+# son2 = int(input("sonni kiriting : "))
+# if son1<son2:
+#     print(f"{son2} katta")
+# elif son2<son1:
+#     print(F"{son1}katta")
+# else:
+#     print("ular teng")
+
+# son1 = int(input("sonni kiriting : "))
+# son2 = int(input("sonni kiriting : "))
+# son3 = int(input("sonni kiriting : "))
+# if son1 > son2 and son1> son3:
+#     print(f"{son1}katta")
+# elif son2>son1 and son2 >son3 :
+#     print(f"{son2}katta")
+# elif son3 > son1 and son3 >son2:
+#     print(f"{son3}katta")
+# else:
+#     print("ullar ozaro teng")
+
+# son = int(input("sonni kiriting : "))
+# if son%2 == 0:
+#     print(F"{son}juft son")
+# elif son%2 ==0:
+#     print(f"{son}bu son toq")
+# else:
+#     print("xato")
